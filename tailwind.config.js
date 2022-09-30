b527b8ce46bf08e152b7f0d37236e0f1ae7c2eea
Truth-hash <86872654+Truth-hash@users.noreply.github.com>
2022-09-30 09:59:54 +0000

    project complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        xxs: "0.625rem",
+      },
+      
+    },
   },
   plugins: [],
 };
